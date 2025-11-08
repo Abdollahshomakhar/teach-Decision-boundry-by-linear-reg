f225d7d45345274ccfc6761a8cdd387d78e07d79
Esi <eghorbani361@gmail.com>
2025-11-08 13:31:05 +0330

    Add files via upload

---
+++ Db file/lab_utils.py
@@ -0,0 +1,39 @@
+"""
+lab_utils_common
+   contains common routines and variable definitions
+   used by all the labs in this week.
+   by contrast, specific, large plotting routines will be in separate files
+   and are generally imported into the week where they are used.
+   those files will import this file
+"""
+import copy
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from ipywidgets import Output
+
+np.set_printoptions(precision=2)
+
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+plt.style.use('./deeplearning.mplstyle')
+
+
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors
+    neg = neg.reshape(-1,)
+
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'red', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors=dlblue, lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
